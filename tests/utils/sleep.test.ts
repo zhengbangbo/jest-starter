7876e39a518634c79472eb6576582d49b0ff2935
Bang-Bo Zheng <zhengbangbo@hotmail.com>
2022-06-08 18:18:50 +0800

    理解 JavaScript 的执行顺序

---
+++ tests/utils/sleep.test.ts
@@ -0,0 +1,26 @@
+import sleep from "utils/sleep";
+
+describe('sleep', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  })
+
+  it('可以睡眠 1000ms', async () => {
+    const callback = jest.fn();
+
+    const act = async (callback: () => void) => {
+      await sleep(1000)
+      callback();
+    }
+
+    const promise = act(callback)
+
+    expect(callback).not.toHaveBeenCalled();
+
+    jest.runAllTimers();
+
+    await promise
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  })
+})
