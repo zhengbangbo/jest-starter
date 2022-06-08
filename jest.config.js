7715d5cd0941938fce46678f9ba985e32c8b8014
Bang-Bo Zheng <zhengbangbo@hotmail.com>
2022-06-08 16:38:08 +0800

    路径简写

--- jest.config.js
+++ jest.config.js
@@ -66,9 +66,10 @@ module.exports = {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  // moduleDirectories: [
-  //   "node_modules"
-  // ],
+  moduleDirectories: [
+    "node_modules",
+    "src"
+  ],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
@@ -81,7 +82,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1"
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
